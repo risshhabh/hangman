cf3967454751027d769e5cf65263c07cdcfcf05e
CodingSoda <26rkshah@gmail.com>
2021-12-31 12:09:39 -0800

    Add: Code to choose a single word from `file`.

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from random import choice  # Randomly pick a word.
+
 filename = r"secret.txt"
 
 # Remove all comments from `secret.txt`
 with open(filename, "r+") as f:
-    file = [line.strip() for line in f.read().splitlines()]  # Strip whitespaces.
+    file = [line.strip() for line in f.read().splitlines()]
 
     # Remove all comments from list of lines in `filename`.
     for ind, val in enumerate(file):
         if val[0] == "#":
             file.pop(ind)
+
+
+# Choose word, if file is empty, end program.
+try:
+    word = choice(file)
+except IndexError:
+    print(
+        f"""\
+You haven't chosen a word.
+Please input the words into `{filename}` and rerun the program."""
+    )
+    exit()
