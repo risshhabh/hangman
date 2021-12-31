a22d106c5357c4b816fddb9b89eeb74faa5cb6d9
CodingSoda <26rkshah@gmail.com>
2021-12-31 12:05:08 -0800

    Update `file` to remove whitespaces.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ filename = r"secret.txt"
 
 # Remove all comments from `secret.txt`
 with open(filename, "r+") as f:
-    file = f.read().splitlines()
+    file = [line.strip() for line in f.read().splitlines()]  # Strip whitespaces.
 
     # Remove all comments from list of lines in `filename`.
     for ind, val in enumerate(file):
