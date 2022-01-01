9485adb48f8363640e0d6b67acc10717672cc921
CodingSoda <26rkshah@gmail.com>
2021-12-31 16:35:19 -0800

    Implemented guess system (and finished code)

--- main.py
+++ main.py
@@ -25,3 +25,48 @@ Please input the words into `{filename}` and rerun the program."""
 
 length = len(word)
 guess = list("_" * length)  # Replace "_" with correct characters with every guess.
+
+print("Let's play hangman! Try guessing the word:")
+
+correct = False
+guesses = 6
+
+while guesses > 0:
+    if correct == True:
+        break
+
+    print(*guess, sep=" ")
+    char = input("Enter a character, or the entire word if you know it. \n--> ")
+
+    if char == word:
+        guesses -= 1
+        correct = True
+
+    elif len(char) == length:
+        print("That is not the word! Try again.")
+        guesses -= 1
+
+    elif len(char) != 1:
+        print(
+            "You have entered something more than one character; that is not allowed. Try again."
+        )
+        # Don't subtract from guesses for invalid input.
+
+    elif char in word:
+        for ind, val in enumerate(word):
+            if char == val:
+                guess[ind] = char
+
+                if "".join(guess) == word:
+                    correct = True
+        guesses -= 1
+
+    else:  # if `char` not in word and is one character long.
+        guesses -= 1
+
+
+if correct:
+    print(f"You guessed the word {word} in {6 - guesses} tries!")
+
+else:
+    print(f'The word was "{word}", you weren\'t able to guess it. Maybe next time?')
